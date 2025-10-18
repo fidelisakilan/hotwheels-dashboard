d13f6b81ed4708f68c8491a8d1758687b362b3ca
Fidelis Akilan <fidelisakilan@proton.me>
2025-10-18 17:50:47 -0500

    changes

--- lib/widgets/lap_detail_widget.dart
+++ lib/widgets/lap_detail_widget.dart
@@ -2,9 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:f1_analyzer/model/lap_detail_model.dart';
 import 'dart:math' as math;
 
-class LapDetailWidget extends StatelessWidget {
+enum DetailTabs { visual, temp, pressure }
+
+class LapDetailWidget extends StatefulWidget {
   const LapDetailWidget({super.key, required this.tyre});
   final TyreModel tyre;
+
+  @override
+  State<LapDetailWidget> createState() => _LapDetailWidgetState();
+}
+
+class _LapDetailWidgetState extends State<LapDetailWidget> {
+  var currentTab = DetailTabs.temp;
+
+  void changeTab(DetailTabs tab) {
+    print(tab);
+    setState(() {
+      currentTab = tab;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +30,12 @@ class LapDetailWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          SegmentedButtonWidget(currentTab: currentTab, changeTab: changeTab),
           Text('TYRE ANALYSIS', style: Theme.of(context).textTheme.titleLarge),
           Text(
-            'Track Temperature: ${tyre.trackTemperature}°C',
+            'Track Temperature: ${widget.tyre.trackTemperature}°C',
             style: Theme.of(context).textTheme.bodyMedium,
           ),
-          const SizedBox(height: 16),
           Expanded(
             child: LayoutBuilder(
               builder: (context, constraints) {
@@ -50,19 +66,19 @@ class LapDetailWidget extends StatelessWidget {
                       children: [
                         _TyreWidget(
                           position: 'Front Left',
-                          tyreDetail: tyre.frontLeft,
+                          tyreDetail: widget.tyre.frontLeft,
                         ),
                         _TyreWidget(
                           position: 'Front Right',
-                          tyreDetail: tyre.frontRight,
+                          tyreDetail: widget.tyre.frontRight,
                         ),
                         _TyreWidget(
                           position: 'Rear Left',
-                          tyreDetail: tyre.rearLeft,
+                          tyreDetail: widget.tyre.rearLeft,
                         ),
                         _TyreWidget(
                           position: 'Rear Right',
-                          tyreDetail: tyre.rearRight,
+                          tyreDetail: widget.tyre.rearRight,
                         ),
                       ],
                     ),
@@ -72,6 +88,42 @@ class LapDetailWidget extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SegmentedButtonWidget extends StatelessWidget {
+  const SegmentedButtonWidget({
+    super.key,
+    required this.changeTab,
+    required this.currentTab,
+  });
+  final Function(DetailTabs) changeTab;
+  final DetailTabs currentTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SegmentedButton<DetailTabs>(
+        segments: const <ButtonSegment<DetailTabs>>[
+          ButtonSegment<DetailTabs>(
+            value: DetailTabs.temp,
+            label: Text('Tyre Temperature'),
+          ),
+          ButtonSegment<DetailTabs>(
+            value: DetailTabs.pressure,
+            label: Text('Tyre Pressure'),
+          ),
+          ButtonSegment<DetailTabs>(
+            value: DetailTabs.visual,
+            label: Text('Tyre Visuals'),
+          ),
+        ],
+        showSelectedIcon: false,
+        selected: <DetailTabs>{currentTab},
+        onSelectionChanged: (Set<DetailTabs> newSelection) =>
+            changeTab(newSelection.first),
       ),
     );
   }
